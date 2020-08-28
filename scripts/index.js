233ff3f756e2f71e5497aa315a38b0eb44256c05
Jon Jessee <tjessee7624@gmail.com>
2020-08-27 21:02:30 -0600

    Tested ability to retrieve sunset and sunrise hours and the daylight hours.

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,11 @@ console.log('Sanity Check');
 //Variable definitions
 
 
+
+
+let sunset= $('#sunset');
+let sunrise = $('#sunrise');
+let dayLength = $('#dayLength');
 //Event listener for the searchbar
 
 //https://ridb.recreation.gov/api/v1/Endpoint?parms
@@ -17,14 +22,18 @@ console.log('Sanity Check');
     //Sunrise and sunset times
     //Length of daylight hours
     
-    //User must input lat and lng and date formatted YYYY-MM-DD
-    let queryURL = 'https://api.sunrise-sunset.org/json?lat=' + lat + 'lng=' + lng + 'date=' + date;
+//User must input lat and lng and date formatted YYYY-MM-DD
+// let queryURL = 'https://api.sunrise-sunset.org/json?lat=' + lat + 'lng=' + lng + 'date=' + date;
+let queryURL = 'https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=2020-08-28';
 $.ajax({
     url: queryURL,
     method: 'GET'
 }).then(function(response) {
     console.log(response);
-
+    console.log(response.results.sunrise);
+    console.log(response.results.sunset);
+    console.log(response.results.day_length);
 });
+
 
 
