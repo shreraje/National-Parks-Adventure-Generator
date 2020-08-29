c56cad88d2a48b6abdae77b33f99d50b327d9731
Jon Jessee <tjessee7624@gmail.com>
2020-08-29 10:08:19 -0600

    Created second ajax call and corrected the Materialize CSS linking.

--- scripts/index.js
+++ scripts/index.js
@@ -10,7 +10,17 @@ console.log('Sanity Check');
     //URLs for the navbar
     //General park info
     //Acitivites
-
+    //curl -H 'X-Api-Key: INSERT-API-KEY-HERE' 
+    //API Key: CIOegTmdfiM4Yf3b17p4OpcSRxRf0G6lZ4pgTuOv
+    //Ex call: https://developer.nps.gov/api/v0/parks?stateCode=DC,MD,VA&fields=images
+let activity = 'hiking';
+let queryURL1 = 'https://developer.nps.gov/api/v1/activities?q=' + activity + '&api_key=CIOegTmdfiM4Yf3b17p4OpcSRxRf0G6lZ4pgTuOv';
+$.ajax({
+    url: queryURL1,
+    method: 'GET'
+}).then(function(response1) {
+    console.log(response1)
+});
 
 
 //Sunset/sunrise Ajax request
@@ -18,13 +28,13 @@ console.log('Sanity Check');
     //Length of daylight hours
     
     //User must input lat and lng and date formatted YYYY-MM-DD
-    let queryURL = 'https://api.sunrise-sunset.org/json?lat=' + lat + 'lng=' + lng + 'date=' + date;
-$.ajax({
-    url: queryURL,
-    method: 'GET'
-}).then(function(response) {
-    console.log(response);
+    // let queryURL = 'https://api.sunrise-sunset.org/json?lat=' + lat + 'lng=' + lng + 'date=' + date;
+// $.ajax({
+//     url: queryURL,
+//     method: 'GET'
+// }).then(function(response) {
+//     console.log(response);
 
-});
+// });
 
 
